d7b8a9aca907dd7df45777960305b2e814b31f90
Riki Konikoff <riki@workbar.com>
2017-11-17 11:52:50 -0500

    google is grabbing my sheet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import gapi from 'gapi-client';
+import Dancers from 'components/Dancers/index';
 import './index.css';
 
+const spreadsheetId = process.env.REACT_APP_SPREADSHEET_KEY
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      googleSpreadsheet: null
+    }
+    this.initializeGoogle = this.initializeGoogle.bind(this);
+  };
+
+  initializeGoogle() {
+    gapi.client.init({
+      'apiKey': apiKey
+    }).then(() => {
+      return gapi.client.request({
+        'path': `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}`
+      })
+    }).then((response) => {
+      console.log(response.result);
+      this.setState({ googleSpreadsheet: response.result });
+    }, (reason) => {
+      console.log('Error: ' + reason.result.error.message);
+    });
+  };
+
+  componentDidMount() {
+    gapi.load('client', this.initializeGoogle);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="Zikukim">Welcome to the Zikukim Homepage</h1>
+          <Dancers googleSpreadsheet={this.state.googleSpreadsheet} />
         </header>
       </div>
     );
--- src/components/Dancers/index.js
+++ src/components/Dancers/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import GoogleSpreadsheet from 'google-spreadsheet';
-import { doc, dancerSheet } from './processProps';
 
-const Dancers = ({
-  doc,
-  dancerSheet
-}) => {
-  return (
-    <div>
-      I am the dancers page
-    </div>
-  )
+class Dancers extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        I am the dancer page
+      </div>
+    )
+  }
 }
 
 export default Dancers;
